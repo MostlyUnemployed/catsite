079913d715592064ff71d5300cf6b6ba5889d962
Alexander Ramsey <alexanderjohnramsey@gmail.com>
2018-09-25 19:53:57 +1000

    meow

---
+++ game.js
@@ -0,0 +1,15 @@
+// Avaliable globals:
+// ctx (rendering context)
+// width, height (the width and height of the window)
+
+const drawBackground = () => {
+  ctx.fillRect(0, 0, width, height)
+}
+
+
+
+function frame() {
+
+}
+
+frame()
